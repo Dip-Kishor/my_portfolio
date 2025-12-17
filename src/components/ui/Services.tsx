762e8c1486d33674cf058203e594657efaec762f
Dip-Kishor <dipkishor9910@gmail.com>
2025-12-17 22:49:24 +0545

    Minor Ui changes in Services card

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -1,17 +1,17 @@
 const Services = () => {
     return (
-        <div className="-mt-10  md:mt-20  text-center">
+        <div className="mt-15  md:mt-20  text-center">
             <h3 className="text-text text-4xl font-bold">Services</h3>
             <h4 className="text-muted-text">Helping You Build Modern Web Solutions</h4>
 
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8 mx-8 md:mx-20 justify-items-center">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8 mx-1 md:mx-20 justify-items-center">
                 <div className="bg-cards p-5 rounded-2xl text-center w-full max-w-xs">
                     <img
                         src="/Images/WebDesign.jpg"
                         alt="Web App Development"
                         className="w-full h-30 md:h-48 object-cover rounded-lg mb-4"
                     />
-                    <h3 className="text-button text-2xl font-bold mb-2">Web App Development</h3>
+                    <h3 className="text-button text-xl md:text-2xl font-bold mb-2">Web App Development</h3>
                     <p className="text-sm text-muted-text">
                         Full-stack web apps with reliability and fast functioning.
                     </p>
@@ -24,7 +24,7 @@ const Services = () => {
                         alt="Frontend Development"
                         className="w-full h-30 md:h-48 object-cover rounded-lg mb-4"
                     />
-                    <h3 className="text-button text-2xl font-bold mb-2">Frontend Development</h3>
+                    <h3 className="text-button text-xl md:text-2xl font-bold mb-2">Frontend Development</h3>
                     <p className="text-sm text-muted-text">
                         Build responsive and interactive user interfaces using modern frameworks.
                     </p>
@@ -37,7 +37,7 @@ const Services = () => {
                         alt="Backend Development"
                         className="w-full h-30 md:h-48 object-cover rounded-lg mb-4"
                     />
-                    <h3 className="text-button text-2xl font-bold mb-2">Backend Development</h3>
+                    <h3 className="text-button text-xl md:text-2xl font-bold mb-2">Backend Development</h3>
                     <p className="text-sm text-muted-text">
                         Server-side programming, database management, and API development.
                     </p>
@@ -49,7 +49,7 @@ const Services = () => {
                         alt="E-commerce Solutions"
                         className="w-full h-30 md:h-48 object-cover rounded-lg mb-4"
                     />
-                    <h3 className="text-button text-2xl font-bold mb-2">E-commerce Solutions</h3>
+                    <h3 className="text-button text-xl md:text-2xl font-bold mb-2">E-commerce Solutions</h3>
                     <p className="text-sm text-muted-text">
                         Online stores with payment gateways, shopping carts, and product management.
                     </p>
@@ -61,7 +61,7 @@ const Services = () => {
                         alt="API Integration"
                         className="w-full h-30 md:h-48 object-cover rounded-lg mb-4"
                     />
-                    <h3 className="text-button text-2xl font-bold mb-2">API Integration</h3>
+                    <h3 className="text-button text-xl md:text-2xl font-bold mb-2">API Integration</h3>
                     <p className="text-sm text-muted-text">
                         Connect third-party APIs like payment, maps, social media, or analytics.
                     </p>
